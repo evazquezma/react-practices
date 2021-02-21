98a4a36c1b955b20a14ae0d916284d689a615e33
evazquezma <evazquezma@indra.es>
2021-02-21 19:38:09 +0100

    burger-app: Configurable burger builder

--- burger-app/src/components/Burger/Burger.js
+++ burger-app/src/components/Burger/Burger.js
@@ -3,13 +3,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-
-
-   return (
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map((ingredientKey) => {
+            return [...Array(props.ingredients[ingredientKey])].map((_,i) => {
+                return  <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>;
+            });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el);
+        }, []);
+    console.log(transformedIngredients);
+    if (transformedIngredients.length == 0) {
+        transformedIngredients = <p>Por favor, añada ingredientes</p>
+    }
+    return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-botton"/>
         </div>
    );
